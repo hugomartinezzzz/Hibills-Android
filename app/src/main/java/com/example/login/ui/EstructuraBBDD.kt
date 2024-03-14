23081eda8a1d0c507afbf4f382c422ee15d18ae4
hugo <hugomartinezpriego@gmail.com>
2024-03-14 17:16:59 +0100

    v12

---
+++ app/src/main/java/com/example/login/ui/EstructuraBBDD.kt
@@ -0,0 +1,19 @@
+package com.example.login.ui
+
+data class estructuraBBDD (
+    var correo: String? = null,
+    var nombre: String? = null,
+    var contrasenya: String? = null,
+)
+
+data class estructuraGastos(
+    var idcategoria: String? = null,
+    var idmes: String? = null,
+    var cantidad: String? = null,
+    var nombre: String? = null
+)
+
+data class categorias(
+    var nombreCat: String? = null,
+    var idGasto: Int? = null
+)
